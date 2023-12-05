c26ddd4b21a8660a02115841d3c12a79891b3463
Luaranet <luaranet08@gmail.com>
2023-12-05 10:07:07 -0400

    commit
    arrumei a página cadastro que não estava aparecendo o conteúdo, refiz a propriedade dos videos e a função salvar, adicionei a importação do usestate e useeffect

--- src/Cadastro/Cadastro.jsx
+++ src/Cadastro/Cadastro.jsx
@@ -1,49 +1,40 @@
-import { useEffect, useState } from "react";
-import "./style.css";
-import { Link } from "react-router-dom";
+import  { useEffect, useState } from "react"
 
-export default function ToDo() {
-    const listaLocalStorage = JSON.parse(localStorage.getItem("Lista"));
-    const [titulo, setTitulo] = useState("");
+export default function Cadastro() {
+const [titulo, setTitulo] = useState("");
     const [genero, setGenero] = useState("");
     const [lançamento, setLançamento] = useState("");
     const [diretor, setDiretor] = useState("");
-    const [id, setId] = useState[1];
+    const [id, setId] = useState(1);
     const [faixaEtaria, setFaixaEtaria] = useState("");
     const [link, setLink] = useState("");
-    const [lista, setLista] = useState(listaLocalStorage || []);
-
-
+    const [lista, setLista] = useState([]);
 
     const salvar = (e) => {
-        e.preventDefault();
-        setLista([...lista, {
-            atividade: atividade,
-            id: id
-        }]);
-        setId(id + 1);
-        setAtividade("");
-    };
-    
-    return (
-        <div class="container">
-            <h1>Lista de Atividades</h1>
-            <form onSubmit={salvar}>
-                <input type="text"
-                    value={atividade}
-                    onChange={(e) => { setAtividade(e.target.value) }} />
-                <button>ADICIONAR</button>
-            </form>
-            {lista.map((ativ) =>
-                <ul key={ativ.id}>
-                    <li>
-                        <Link to={`/detalhe/${ativ.id}`}>                  
-                              <p>{ativ.atividade}</p>
-                        </Link>
-                        <button onClick={() => remover(ativ.id)}>Remover</button>
-                    </li>
-                </ul>
-            )}
-        </div>
-    );
-}
+        e.preventDefault()
+            setLista([...lista,{
+                titulo: titulo,
+                genero: genero, 
+                lançamento: lançamento,
+                diretor: diretor, 
+                id: id,
+                faixaEtaria: faixaEtaria,
+                link: link,
+                lista: lista
+            }])
+
+            setTitulo("")
+            setGenero("")
+            setLançamento("")
+            setDiretor("")
+            setId("")
+            setFaixaEtaria("")
+            setLink("")
+            setLista("")
+        }
+
+        return(
+            <h1>Ola</h1>
+        );
+
+    }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import ToDo from './Cadastro/Cadastro';
-import Detalhe from './Detalhe/Detalhe';
+import Cadastro from './Cadastro/Cadastro';
 import Home from './Home/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>   
-        <Route path='/' element={<ToDo />}></Route>
-        <Route path='/detalhe/:id' element={<Detalhe  />}></Route>
+        <Route path='/cadastro' element={<Cadastro />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
