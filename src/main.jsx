ed19c2849e605b55826477bd859712b9bd4b6f81
Luaranet <luaranet08@gmail.com>
2023-12-07 20:50:06 -0300

    commit
    rota da página detalhe

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Cadastro from './Cadastro/Cadastro';
 import Home from './Home/Home'
 import Destaques from './Destaques/Destaques'
+import Detalhe from './Detalhe/detalhe'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,6 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>   
         <Route path='/cadastro' element={<Cadastro />}></Route>
         <Route path='/destaques' element={<Destaques/>}></Route>
+        <Route path='/detalhe/:id' element={<Detalhe/>}></Route>
+
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
