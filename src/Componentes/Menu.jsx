5dcdcf9243ae897707d08a179c5c4a6e3902a21f
Luaranet <luaranet08@gmail.com>
2023-12-10 21:36:03 -0300

    commit
    fiz o menu

--- src/Componentes/Menu.jsx
+++ src/Componentes/Menu.jsx
@@ -1,29 +1,45 @@
 import { Link } from "react-router-dom";
+  import "../styles/menu.css"
 
 export default function Menu() {
     return (
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
+
+      <nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse  justify-content-center  " id="navbarNav">
+      <ul class="navbar-nav">
+
             <li class="nav-item">
-             <p><Link to={'/'}>Home</Link></p>
+           
+            <Link to={'/'}>
+              <p> <img src="../imagens/home.png"></img>Home</p>
+            </Link>
             </li>
-    
+
+
             <li class="nav-item">
-             <p><Link to={'/cadastro'}>Cadastrar</Link></p>
+           
+           <Link to={'/cadastro'}>
+             <p> <img src="../imagens/cadastrar.png"></img>Cadastrar</p>
+           </Link>
+           </li>
+
+           <li class="nav-item">
+           
+            <Link to={'/destaque'}>
+              <p> <img src="../imagens/destaque.png"></img>Destaques</p>
+            </Link>
             </li>
-    
-            <li class="nav-item">
-             <p><Link to={'/destaques'}>Destaques</Link></p>
-            </li>
-    
-          </ul>
-        </div>
-      </div>
-    </nav>
+
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
   );
 }
